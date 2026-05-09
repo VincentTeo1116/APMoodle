cdd531e798d461d132de9f422031bfdccaee4d02
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-05-09 12:11:44 +0800

    Fix folder structure: correct root level with Dockerfile

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490bffaeaf40cb0591414319fdbbcf852850355c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228932ec4b4d47b0689b4ac6b0676814dea576c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
