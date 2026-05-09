8875365f1c9e7d1905a3ea7251f5e2c195c31fc1
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-05-09 16:37:45 +0800

    Change the solution file

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a96a3bda51b0898dd3b61f12e90e4c8fe2bb69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22582a2ceef15c96fc0e8662b13eef0ba427deed")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
