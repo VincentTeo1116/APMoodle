224e28ad60f1a639f51704ead4c93d91a056aaa3
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-05-09 16:59:17 +0800

    Change the title of the pages:

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8875365f1c9e7d1905a3ea7251f5e2c195c31fc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e34f9e9ac7734f148a4990321ee6212d0d1d9313")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
