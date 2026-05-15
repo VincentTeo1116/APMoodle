1aa297ab8f9c8e11bb680e61540cf8a65c0bf121
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-05-16 02:28:09 +0800

    Add profile page

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b9908a99f69d6b5b81f31d2250434f47ff00c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b427faf34280fb088aca1c79a5dd8ce309287e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
