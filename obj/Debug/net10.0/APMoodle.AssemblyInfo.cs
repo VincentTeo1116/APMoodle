5a71dda4a9f8bd1eee2e00963185e6894efb59f4
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-05-21 21:02:22 +0800

    Quiz UI was added

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60855293088713085104cf70e192385b7f5e9944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b1a07d98619c369dc09d1bcb09e2ea4a0a8f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
