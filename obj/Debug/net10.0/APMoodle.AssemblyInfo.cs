b2dc35b12f7a18fbb4f06a0e66e069dd19139cbe
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-07-18 13:01:48 +0800

    admin dashboard concept changed

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e5d6cb109a16bc62f074b90998bbbcfc3054ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fcde7525fe09ef758c5d407ba224cf58a85926")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
