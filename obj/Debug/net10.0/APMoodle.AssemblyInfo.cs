d5cc760d751317b2d8d8e0deda8dabd68fcf8af9
VincentTeo1116 <vincentteo77416560@gmail.com>
2026-07-18 14:08:40 +0800

    add the quiz history page

--- obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
+++ obj/Debug/net10.0/APMoodle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fcde7525fe09ef758c5d407ba224cf58a85926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2dc35b12f7a18fbb4f06a0e66e069dd19139cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APMoodle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
